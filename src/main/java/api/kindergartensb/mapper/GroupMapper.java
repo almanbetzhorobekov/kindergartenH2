37139de4d04a5e446166a54b8b9ea4a554dbc906
azh <almanbetzhorobekov@veda.net>
2025-06-24 16:59:36 +0200

    Project updated with new Branch  24.06.2025

--- src/main/java/api/kindergartensb/mapper/GroupMapper.java
+++ src/main/java/api/kindergartensb/mapper/GroupMapper.java
@@ -1,15 +1,21 @@
 package api.kindergartensb.mapper;
 
 
-import api.kindergartensb.dto.GroupDTO;
 import api.kindergartensb.entity.Group;
+import api.kindergartensb.dto.GroupDTO;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 
 @Mapper(componentModel = "spring")
 public interface GroupMapper {
 
+    @Mapping(source = "kindergarten.uuid", target = "kindergartenId")
+    @Mapping(source = "educator.uuid", target = "educatorId")
     GroupDTO toDto(Group group);
-    Group toEntity(GroupDTO groupDTO);
+
+    @Mapping(target = "kindergarten", ignore = true)
+    @Mapping(target = "educator", ignore = true)
+    Group toEntity(GroupDTO dto);
 
 }
